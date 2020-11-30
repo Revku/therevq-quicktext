0eea5a4886ab34eab514d3a0eb9f93c4a3bd53bf
TheRevQ <biznes.therevq@gmail.com>
2020-11-30 21:23:12 +0100

    v1.0.0 Version, initial commit

---
+++ src/js/index.js
@@ -0,0 +1,57 @@
+const area = document.getElementById('haste');
+
+const iconInfo = document.getElementById('nav__info');
+const iconClear = document.getElementById('nav__clear');
+const heroImage = document.querySelector('img');
+
+const localStorageStatus = true;
+
+const goTo = (url) => {
+    location.href = url;
+}
+
+const goToNew = (url) => {
+    const win = window.open(url, '_blank');
+    win.focus();
+}
+
+const testLocalStorage = () => {
+    try {
+        localStorage.setItem('test', 'test');
+        localStorage.removeItem('test');
+    } catch {
+        localStorageStatus = false;
+        console.error('Saving haste failed! LocalStorage is not defined.');
+    }
+}
+
+const saveHaste = (text) => {
+    localStorage.setItem('hasteValue', text);
+}
+
+const loadHaste = () => {
+    const hValue = localStorage.getItem('hasteValue');
+    area.value = hValue;
+}
+
+const clearHaste = () => {
+    area.value = " ";
+    localStorage.removeItem("hasteValue");
+}
+
+area.addEventListener('input', () => {
+    saveHaste(area.value);
+});
+
+iconClear.addEventListener('click', () => {
+    clearHaste()
+});
+iconInfo.addEventListener('click', () => {
+    goToNew('./about');
+});
+
+heroImage.addEventListener('click', () => {
+    goTo('https://therevq.pl');
+});
+
+loadHaste();
